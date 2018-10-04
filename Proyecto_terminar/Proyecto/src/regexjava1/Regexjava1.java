c1b2e84bcc1ee4c32329e948eb066abec7965aa3
DaniDaniel88 <danielsmith6669@hotmail.com>
2018-10-04 10:19:53 -0500

    ProyectoHoy
    Actividad

---
+++ Proyecto_terminar/Proyecto/src/regexjava1/Regexjava1.java
@@ -0,0 +1,14 @@
+
+package regexjava1;
+
+public class Regexjava1 {
+    
+    public static void main(String[] args) {
+        
+        AbrirArchivoVentana i = new  AbrirArchivoVentana ();
+        i.setLocationRelativeTo(null);
+        i.setVisible(true);
+        
+    }
+    
+}
